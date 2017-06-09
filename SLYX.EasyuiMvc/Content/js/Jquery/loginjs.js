38d9c9fedb1dad3b030244c2027c19c88069df87
slyx2017 <208106500@qq.com>
2017-06-09 15:02:03 +0800

    Loign

---
+++ SLYX.EasyuiMvc/Content/js/Jquery/loginjs.js
@@ -0,0 +1,17 @@
+﻿function Login() {
+
+        $.ajax({
+            url: "/User/CheckLogin",
+            type: "post",
+            success: function (data) {
+                
+                if (data.Statu == "ok") {
+                    window.location.href = "/Home/Index";
+                }
+                else {
+                    $.messager.alert("系统提示", "登录失败", "warning");
+                    return;
+                }
+            }
+        })
+}
---
+++ SLYX.EasyuiMvc/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SLYX.EasyuiMvc.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        // GET: User
+        public JsonResult CheckLogin()
+        {
+            SLYX.Common.AjaxMsgModel ajaxMsg = new Common.AjaxMsgModel() { Statu = "err", Msg = "登录失败！" };
+
+            ajaxMsg.Statu = "ok";
+            ajaxMsg.Msg = "登录成功！";
+            return Json(ajaxMsg, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
